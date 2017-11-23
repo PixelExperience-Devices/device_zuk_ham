e5cc075f0ad19b642217dc01f44465c73a13a22d
sb6596 <shubhamprince111@gmail.com>
2017-11-23 06:18:50 +0000

    ham: AOSP-ify (AEx)
    Signed-off-by: ShubhamB6596 <shubhamprince111@gmail.com>
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>

--- aosp_ham.mk
+++ aosp_ham.mk
@@ -19,10 +19,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ham device
 $(call inherit-product, device/zuk/ham/ham.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AEX stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
-PRODUCT_NAME := lineage_ham
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# AospExtended-BuildType
+EXTENDED_BUILD_TYPE := OFFICIAL
+
+PRODUCT_NAME := aosp_ham
 PRODUCT_DEVICE := ham
 PRODUCT_MANUFACTURER := ZUK
 PRODUCT_MODEL := ZUK Z1
