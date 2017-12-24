932d9649a5373d9540801b90df8af3970b88c0c8
ShubhamB6596 <shubhamprince111@gmail.com>
2017-12-24 14:44:12 +0000

    ham: Enable SEPolicy but build permissive
    Signed-off-by: ShubhamB6596 <shubhamprince111@gmail.com>
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -204,9 +204,9 @@ endif
 
 # SELinux policies
 # qcom sepolicy
-#include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/sepolicy.mk
 
-#BOARD_SEPOLICY_DIRS += \
-#    $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += \
+    $(DEVICE_PATH)/sepolicy
 
 -include vendor/zuk/ham/BoardConfigVendor.mk
