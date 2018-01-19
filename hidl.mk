3d0d19b2432110dc1a0a3bd06d4c0a83017dad81
Artem Borisov <dedsa2002@gmail.com>
2018-01-19 15:24:13 +0000

    ham: Enable binderized services for HIDL HALs
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -24,11 +24,14 @@
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl 
+    android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
 
 # Camera
 PRODUCT_PACKAGES += \
@@ -49,7 +52,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.memtrack@1.0-impl 
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service 
 
 # GPS
 PRODUCT_PACKAGES += \
@@ -68,11 +72,13 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl 
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
 
 # Power
 PRODUCT_PACKAGES += \
@@ -84,11 +90,13 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # Wifi
 PRODUCT_PACKAGES += \
