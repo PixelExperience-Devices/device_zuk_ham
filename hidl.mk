3befa86a9fd1ba9300fc7444982c957930a47fea
Bruno Martins <bgcngm@gmail.com>
2018-01-19 15:42:00 +0000

    ham: Convert lights HAL into a native binderized HAL
     * original work for xiaomi-msm8996 device, adopted for ham
     * address selinux denial
    
    Change-Id: If41458e22b9a67a5c2a415571723917df1904aa1
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -79,8 +79,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.light@2.0-service.ham
 
 # Power
 PRODUCT_PACKAGES += \
