05fe344d7eb1561013da20f0d0eae9b35b13a4cb
ShubhamB6596 <shubhamprince111@gmail.com>
2018-01-23 15:21:11 +0000

    Prepare for PixelExperience-OFFICIAL
    Signed-off-by: ShubhamB6596 <shubhamprince111@gmail.com>

--- aosp_ham.mk
+++ aosp_ham.mk
@@ -22,14 +22,16 @@ PRODUCT_COPY_FILES += device/zuk/ham/apns-full-conf.xml:system/etc/apns-conf.xml
 # Inherit from ham device
 $(call inherit-product, device/zuk/ham/ham.mk)
 
-# Inherit some common AEX stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common Pissel stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Boot animation
+# Pissel Stuff
+TARGET_ARCH := arm
+TARGET_DENSITY := xxhdpi
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# AospExtended-BuildType
-EXTENDED_BUILD_TYPE := OFFICIAL
+# PixelExperience-BuildType
+CUSTOM_BUILD_TYPE := OFFICIAL
 
 PRODUCT_NAME := aosp_ham
 PRODUCT_DEVICE := ham
