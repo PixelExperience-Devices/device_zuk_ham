5085a266291f832566cf2c5e3c8753559d69a453
ShubhamB <shubhamprince111@gmail.com>
2018-05-10 08:38:09 +0000

    ham: camera: Move remaining camera modules to vendor
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>

--- camera/QCamera2/stack/mm-jpeg-interface/Android.mk
+++ camera/QCamera2/stack/mm-jpeg-interface/Android.mk
@@ -37,5 +37,6 @@ LOCAL_SRC_FILES := \
 LOCAL_MODULE := libmmjpeg_interface
 LOCAL_SHARED_LIBRARIES := libdl libcutils liblog libqomx_core
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
